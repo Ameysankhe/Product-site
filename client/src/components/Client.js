195291e616e5a3b2c594decc8c96919a2999c565
Amey <amey@mysite.com>
2024-03-04 22:00:55 +0530

    Clients page updated

--- client/src/components/Client.js
+++ client/src/components/Client.js
@@ -14,45 +14,45 @@ function Client() {
         </div>
 
         <div className="box-inside"  data-aos="fade-up">
-          <img src={require("../assets/imagica_image.jpg")} alt="" />
-          <h3>Imagica</h3>
-          <a href="https://www.imagicaaworld.com/theme-park/?utm_source=google_sem&utm_medium=brand_keywords&gclid=CjwKCAjwg4SpBhAKEiwAdyLwvDFNqUoDNl5g_sW4XjV39VG0kwyugvSErzjhLccCG7S3vYLbzYCRGRoCpV8QAvD_BwE" className="btn-info">Read more</a>
+          <img src={require("../assets/nestle.png")} alt="" style={{height:"70px"}} />
+          <h3>Nestle</h3>
+          <a href="https://www.nestle.com/" className="btn-info">Read more</a>
         </div>
 
         <div className="box-inside"  data-aos="fade-up">
-          <img src={require("../assets/pizzaexpress-image.png")} alt="" />
-          <h3>Pizza express </h3>
-          <a href="https://www.pizzaexpress.com/" className="btn-info">Read more</a>
+          <img src={require("../assets/mcdonalds.png")} alt="" />
+          <h3>McDonald's</h3>
+          <a href="https://www.mcdonalds.com/us/en-us/about-us.html" className="btn-info">Read more</a>
         </div>
 
         <div className="box-inside"  data-aos="fade-up">
-          <img src={require("../assets/reliance-image.jpg")} alt="" />
-          <h3>Reliance</h3>
-          <a href="https://www.ril.com/" className="btn-info">Read more</a>
+          <img src={require("../assets/ms-navkar.png")} alt="" style={{height:"60px"}}/>
+          <h4>Navkar Corporation Ltd</h4>
+          <a href="https://www.navkarcorp.com/" className="btn-info">Read more</a>
         </div>
 
         <div className="box-inside" data-aos="fade-up">
-          <img src={require("../assets/raymond-image.png")} alt="" />
-          <h3>Raymond</h3>
-          <a href="https://www.raymond.in/" className="btn-info">Read more</a>
+          <img src={require("../assets/vnr.png")} alt="" />
+          <h3>VNR</h3>
+          <a href="https://www.vnrseeds.com/" className="btn-info">Read more</a>
         </div>
 
         <div className="box-inside"  data-aos="fade-up">
-          <img src={require("../assets/lilavati-image.jpg")} alt="" />
-          <h3>Lilavati Hopsital</h3>
-          <a href="https://lilavatihospital.com/" className="btn-info">Read more</a>
+          <img src={require("../assets/lupin.png")} alt="" />
+          <h3>Lupin</h3>
+          <a href="https://www.lupin.com/" className="btn-info">Read more</a>
         </div>
 
         <div className="box-inside"  data-aos="fade-up">
-          <img src={require("../assets/rusom-iamge.png")} alt="" />
-          <h3>Rustomjee</h3>
-          <a href="https://www.rustomjee.com/" className="btn-info">Read more</a>
+          <img src={require("../assets/jw-marriott.png")} alt="" />
+          <h3>JW Marriott</h3>
+          <a href="https://jw-marriott.marriott.com/" className="btn-info">Read more</a>
         </div>
 
         <div className="box-inside"  data-aos="fade-up">
-          <img src={require("../assets/syscom-image.png")} alt=""  style={{height:"50px"}}/>
-          <h3>Syscom</h3>
-          <a href="https://syscom.com/" className="btn-info">Read more</a>
+          <img src={require("../assets/glenmark.png")} alt=""  style={{height:"50px"}}/>
+          <h3>Glenmark</h3>
+          <a href="https://glenmarkpharma.com/" className="btn-info">Read more</a>
         </div>
 
         <div className="box-inside"  data-aos="fade-up">
@@ -68,33 +68,33 @@ function Client() {
         </div>
 
         <div className="box-inside"  data-aos="fade-up">
-          <img src={require("../assets/coldstar-image.png")} alt="" />
-          <h3>Cold Star</h3>
-          <a href="https://coldstar.in/" className="btn-info">Read more</a>
+          <img src={require("../assets/faasos.png")} alt="" />
+          <h3>Faasos</h3>
+          <a href="https://www.instagram.com/faasos/?hl=en" className="btn-info">Read more</a>
         </div>
 
         <div className="box-inside"  data-aos="fade-up">
-          <img src={require("../assets/blue-image.png")} alt="" style={{height:"50px"}}/>
-          <h3>Blue Star</h3>
-          <a href="https://www.bluestarindia.com/" className="btn-info">Read more</a>
+          <img src={require("../assets/tyson.png")} alt="" style={{height:"50px"}}/>
+          <h3>Tyson</h3>
+          <a href="https://www.tysonfoods.com/" className="btn-info">Read more</a>
         </div>
 
         <div className="box-inside"  data-aos="fade-up">
-          <img src={require("../assets/dhl-image.png")} alt="" style={{height:"50px"}} />
-          <h3>DHL</h3>
-          <a href="https://www.dhl.com/in-en/home.html?locale=true" className="btn-info">Read more</a>
+          <img src={require("../assets/rotex.png")} alt="" style={{height:"50px"}} />
+          <h3>Rotex</h3>
+          <a href="https://rotexautomation.com/" className="btn-info">Read more</a>
         </div>
 
         <div className="box-inside"  data-aos="fade-up">
-          <img src={require("../assets/hinduja-hospital-image.jpg")} alt="" />
-          <h3>Hinduja Hospital</h3>
-          <a href="https://www.hindujahospital.com/" className="btn-info">Read more</a>
+          <img src={require("../assets/lodha.png")} alt=""  style={{height:"45px"}}/>
+          <h3>Lodha</h3>
+          <a href="https://www.lodhagroup.in/home/" className="btn-info">Read more</a>
         </div>
 
         <div className="box-inside">
-          <img src={require("../assets/johnson-image.png")} alt="" />
-          <h3>Johnson & Johnson</h3>
-          <a href="https://www.jnj.in/" className="btn-info">Read more</a>
+          <img src={require("../assets/ig-pvt-ltd.png")} alt="" />
+          <h3>IG pvt ltd</h3>
+          <a href="https://www.iginternational.net/" className="btn-info">Read more</a>
         </div>
 
       </div>
